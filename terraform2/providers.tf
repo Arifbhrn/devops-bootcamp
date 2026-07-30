a60cd4939763838d310c54fab207d313813695ea
Nik <arifbhrn0@gmail.com>
2026-07-30 21:57:01 +0800

    add remote backend

--- terraform2/providers.tf
+++ terraform2/providers.tf
@@ -6,10 +6,18 @@ terraform {
       version = "~> 6.0"
     }
   }
+  
+    backend "s3" {
+    bucket       = "bootcamp-2026-aoki"
+    key          = "terraform2/terraform.tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
+  }
 }
+
 
 provider "aws" {
   region = "ap-southeast-1"
 }
 
-data "aws_caller_identity" "my_account" {}
+data "aws_caller_identity" "my_account" {} 
