6f856b36cce35f05e6b0a50ea82b169a49c7c337
Arif-dev00 <125997728+Arifbhrn@users.noreply.github.com>
2026-08-07 02:28:23 +0800

    Terraform4 (#15)

---
+++ terraform4/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "ssm" {
+  name = "tf-rackula-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = { Name = "tf-rackula-ssm-role" }
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+  name = "tf-rackula-ssm-profile"
+  role = aws_iam_role.ssm.name
+}
